c07418deb828dbf1878ca8daf803ca72ce0c26f7
Howard-P <m91623691@gmail.com>
2023-11-27 13:33:47 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2c4678e5-2510-4a61-88b8-85eb5d719e05';
+export const MODAL_CLOSE_SEC = 2.5;
